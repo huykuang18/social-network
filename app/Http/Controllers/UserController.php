d0c3f20e32f98fc7d6897195c14df8bc6b44e12f
Huy NQ <james.realcf@gmail.com>
2021-05-06 09:20:24 +0700

    add register-blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,13 +22,22 @@ class UserController extends BaseController
 
     public function register(Request $request)
     {
-        $user = $this->userRepository->create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]);
-            
-        return $this->sendSuccess('User created successfully',$user,200);
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+        $repassword = $request->repassword;
+        if (User::where('email', '=', $email)->exists()) {
+            return $this->sendSuccess('Email has existed!',null, 401);
+        } elseif($password != $repassword) {
+            return $this->sendSuccess('Confirm password is not true!',null, 401);
+        } else {
+            $user = $this->userRepository->create([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($password)
+            ]);
+            return $this->sendSuccess('User created successfully', $user, 200);
+        }
     }
 
     public function login(Request $request)
@@ -37,24 +46,31 @@ class UserController extends BaseController
         $token = null;
         try {
             if (!$token = JWTAuth::attempt($credentials)) {
-                return $this->sendSuccess('invalid_email_or_password',[],422);
+                return $this->sendSuccess('invalid_email_or_password', [], 422);
             }
         } catch (JWTException $e) {
-            return $this->sendSuccess('failed_to_create_token',[],500);
+            return $this->sendSuccess('failed_to_create_token', [], 500);
         }
-        return $this->sendSuccess('Login successfully!',compact('token'),200);
+        return $this->sendSuccess('Token is created', compact('token'), 200);
     }
 
     public function getUserInfo(Request $request)
     {
         $user = JWTAuth::toUser($request->token);
-        return $this->sendSuccess('Information',$user,200);
+        return $this->sendSuccess('Information', $user, 200);
     }
 
-    function logout()
+    function logout(Request $request)
     {
-        JWTAuth::logout();
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
 
-        return $this->sendSuccess('Successfully logged out');
+        try {
+            JWTAuth::invalidate($request->token);
+            return $this->sendSuccess('User logged out successfully!', true, 200);
+        } catch (JWTException $e) {
+            return $this->sendSuccess('Sorry, the user cannot be logged out', false, 500);
+        }
     }
 }
